17ce9800544cb07a0c932e26e07ab537e1358565
Jon Champagne <jayseejc3191@gmail.com>
2015-06-24 15:47:09 -0400

    Added option for google play services open source licenses.

--- app/src/main/java/com/jayseeofficial/materialmusic/activity/LicensesActivity.java
+++ app/src/main/java/com/jayseeofficial/materialmusic/activity/LicensesActivity.java
@@ -1,6 +1,10 @@
 package com.jayseeofficial.materialmusic.activity;
 
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
 
 import com.jayseeofficial.materialmusic.R;
 
@@ -10,15 +14,41 @@ import de.psdev.licensesdialog.LicensesDialog;
  * Created by jon on 24/06/15.
  */
 public class LicensesActivity extends BaseActivity {
+    public static final String ACTION_OPEN_SOURCE_LICENSES = "com.jayseeofficial.intent.OPEN_SOURCE_LICENSES";
+    public static final String ACTION_GOOGLE_LICENSES = "com.jayseeofficial.intent.GOOGLE_LICENSES";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        LicensesDialog dialog = new LicensesDialog.Builder(this)
-                .setNotices(R.raw.notices)
-                .setShowFullLicenseText(true)
-                .setIncludeOwnLicense(true)
-                .build();
-        dialog.setOnDismissListener(dialog1 -> finish());
-        dialog.show();
+        if (getIntent().getAction().equalsIgnoreCase(ACTION_OPEN_SOURCE_LICENSES)) {
+            LicensesDialog dialog = new LicensesDialog.Builder(this)
+                    .setNotices(R.raw.notices)
+                    .setShowFullLicenseText(true)
+                    .setIncludeOwnLicense(true)
+                    .build();
+            dialog.setOnDismissListener(dialog1 -> finish());
+            dialog.show();
+        } else if (getIntent().getAction().equalsIgnoreCase(ACTION_GOOGLE_LICENSES)) {
+            // Setup dialog contents
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.MATCH_PARENT);
+            ScrollView scrollView = new ScrollView(this);
+            scrollView.setLayoutParams(params);
+
+            TextView textView = new TextView(this);
+            textView.setLayoutParams(params);
+            // TODO uncomment this once play services are enabled
+            //textView.setText(GoogleApiAvalability.getOpenSourceSoftwareLicenseInfo(this));
+            String s = "COMING SOON!\n";
+            for (int i = 0; i < 10; i++) s += s;
+            textView.setText(s);
+            scrollView.addView(textView);
+
+            new AlertDialog.Builder(this)
+                    .setView(scrollView)
+                    .setNegativeButton("Close", (dialog, which) -> finish())
+                    .setOnDismissListener(dialog1 -> finish())
+                    .show();
+        }
     }
 }
